6b6639d835cb0735b9d030db36e5a4f47df172e7
smicl11 <bryan.smith094@gmail.com>
2016-05-05 10:21:10 -0700

    maybe

--- conflict_app/app/controllers/mouse_controller.rb
+++ conflict_app/app/controllers/mouse_controller.rb
@@ -1,10 +1,10 @@
 class MouseController < ApplicationController
   def i_am_the_best_controller_ever
-    puts "I'm awesome! No your not"
+    puts "I'm really really awesome! No your not"
   end
 
   def i_am_the_best_controller_ever
-    puts "okay"
+    puts "okay this is the better way."
   end
 
 end
