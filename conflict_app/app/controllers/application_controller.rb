fa0d1328f08f2bd5167e0b20fd27998d32234040
relative-rene <rene.arellano415@gmail.com>
2016-05-05 10:13:17 -0700

    masterful work

--- conflict_app/app/controllers/application_controller.rb
+++ conflict_app/app/controllers/application_controller.rb
@@ -3,3 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+
+def index
+  
+
+end
+
+def mouse
+  return "i am a mouse"
+end
